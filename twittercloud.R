377c93420e51e6889ec74245888f2688d6374ada
mzhang13 <mzhangthirteen@gmail.com>
2016-08-10 23:12:52 -0400

    Added initial files: R script that pulls a user's tweets and generates a wordcloud, along with basic instructions and .gitignore (for config).

---
+++ twittercloud.R
@@ -0,0 +1,29 @@
+source("config.R")
+library(twitteR)
+library(purrr)
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+
+# set up OAuth
+setup_twitter_oauth(getOption("twitter_consumer_key"),
+                    getOption("twitter_consumer_secret"),
+                    getOption("twitter_access_token"),
+                    getOption("twitter_access_token_secret"))
+
+# Replace string with Twitter handle of interest
+handle <- "mzhang13"
+
+# put username in as first argument
+raw <- userTimeline(handle, n = 3200)
+tweets <- map_df(raw, as.data.frame)
+
+tweets$text <- lapply(tweets$text, function(x) gsub("@\\S+", "", x))
+tweets$text <- lapply(tweets$text, function(x) gsub("#\\S+", "", x))
+
+corpus <- Corpus(VectorSource(tweets$text))
+corpus <- tm_map(corpus, PlainTextDocument)
+corpus <- tm_map(corpus, removeWords, stopwords("en"))
+corpus <- tm_map(corpus, removePunctuation)
+wordcloud(corpus, max.words = 100, random.order = FALSE, 
+          colors = brewer.pal(12, "Paired")) # add ability to customize colors?
